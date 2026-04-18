215192e335088f5d214eb7b892e5670a2e35d290
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-04-18 20:49:38 +0530

    started application development

---
+++ src/main/java/com/SurgLog/SurgLogApplication.java
@@ -0,0 +1,13 @@
+package com.SurgLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurgLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SurgLog/SurgLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SurgLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurgLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
