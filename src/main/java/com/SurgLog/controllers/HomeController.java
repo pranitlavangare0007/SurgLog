584665c29ae3ab4a8982fb7a9b913f3b501ac4c0
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-04-18 21:08:27 +0530

    setup databse connection

---
+++ src/main/java/com/SurgLog/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.SurgLog.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return " welcome";
+    }
+}
